6e8d22b5cfd8a90c4d70b83d571b09d1c14e2db5
Mister fantastic <karimourkia5@gmail.com>
2026-05-07 00:18:28 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**.dicebear.com',
-      },
-    ],
-  },
 }
 
 module.exports = nextConfig
